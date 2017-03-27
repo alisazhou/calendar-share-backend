e61948813b9bea9d6b0b4fc4b2b55d1411cf38b5
Alisa Zhou <alisa.zwy@gmail.com>
2017-03-27 18:29:52 -0400

    add permissions to Profile

--- profiles/tests/view_test.py
+++ profiles/tests/view_test.py
@@ -21,37 +21,84 @@ def check_profile_is_instance(profile_dict, profile_instance):
     assert profile_dict['phone'] == getattr(profile_instance, 'phone', None)
 
 
-def test_get_profiles_list(client, create_profiles):
+def test_get_profiles_list(admin_client, client, create_profiles):
+    profile1, profile2 = Profile.objects.all()
+
+    # anon user gets 403 forbidden
+    response = client.get('/api/profiles/')
+    assert response.status_code == 403
+
+    # login as normal_user1, can see own profile
+    client.login(username='user1', password='qwerty123')
     response = client.get('/api/profiles/')
     assert response.status_code == 200
-
     profiles_json = json.loads(response.content.decode())
-    assert len(profiles_json) == Profile.objects.count()
+    assert len(profiles_json) == 1
+    check_profile_is_instance(profiles_json[0], profile1)
 
-    profile1, profile2 = Profile.objects.all()
+    # login as normal_user2, can see own profile
+    client.logout()
+    client.login(username='user2', password='qwerty123')
+    response = client.get('/api/profiles/')
+    assert response.status_code == 200
+    profiles_json = json.loads(response.content.decode())
+    assert len(profiles_json) == 1
+    check_profile_is_instance(profiles_json[0], profile2)
+
+    # login as admin, can see all profiles
+    response = admin_client.get('/api/profiles/')
+    assert response.status_code == 200
+    profiles_json = json.loads(response.content.decode())
+    assert len(profiles_json) == 2
     check_profile_is_instance(profiles_json[0], profile1)
     check_profile_is_instance(profiles_json[1], profile2)
 
 
-def test_get_profile_by_id(client, create_profiles):
+def test_get_profile_by_id(admin_client, client, create_profiles):
     profile1, profile2 = Profile.objects.all()
 
-    response1 = client.get('/api/profiles/{}/'.format(profile1.user_id))
-    assert response1.status_code == 200
-    profile_json1 = json.loads(response1.content.decode())
-    check_profile_is_instance(profile_json1, profile1)
+    # anon user gets 403 forbidden
+    response = client.get('/api/profiles/{}/'.format(profile1.user_id))
+    assert response.status_code == 403
 
-    response2 = client.get('/api/profiles/{}/'.format(profile2.user_id))
-    assert response2.status_code == 200
-    profile_json2 = json.loads(response2.content.decode())
+    # login as normal_user1, can see own profile
+    client.login(username='user1', password='qwerty123')
+    response = client.get('/api/profiles/{}/'.format(profile1.user_id))
+    assert response.status_code == 200
+    profile_json1 = json.loads(response.content.decode())
+    check_profile_is_instance(profile_json1, profile1)
+    # still can't see others' profiles
+    response = client.get('/api/profiles/{}/'.format(profile2.user_id))
+    assert response.status_code == 404    # 404 because not part of queryset
+
+    # login as normal_user2, can see own profile
+    client.login(username='user2', password='qwerty123')
+    response = client.get('/api/profiles/{}/'.format(profile2.user_id))
+    assert response.status_code == 200
+    profile_json2 = json.loads(response.content.decode())
+    check_profile_is_instance(profile_json2, profile2)
+    # still can't see others' profiles
+    response = client.get('/api/profiles/{}/'.format(profile1.user_id))
+    assert response.status_code == 404    # 404 because not part of queryset
+
+    # admin can see normal_user1's profile
+    response = admin_client.get('/api/profiles/{}/'.format(profile1.user_id))
+    assert response.status_code == 200
+    profile_json1 = json.loads(response.content.decode())
+    check_profile_is_instance(profile_json1, profile1)
+    # admin can also see normal_user2's profile
+    response = admin_client.get('/api/profiles/{}/'.format(profile2.user_id))
+    assert response.status_code == 200
+    profile_json2 = json.loads(response.content.decode())
     check_profile_is_instance(profile_json2, profile2)
 
 
 @pytest.mark.django_db
 def test_post_profiles(
         client, complete_profile_for_view, incomplete_profile_for_view):
-    response1 = client.post('/api/profiles/', complete_profile_for_view)
-    assert response1.status_code == 201
+    # anon users can create profiles
+    response = client.post('/api/profiles/', complete_profile_for_view)
+    assert response.status_code == 201
     assert Profile.objects.count() == 1
 
     response2 = client.post('/api/profiles/', incomplete_profile_for_view)
@@ -62,33 +109,59 @@ def test_post_profiles(
 def test_delete_profiles(client, create_profiles):
     profile1, profile2 = Profile.objects.all()
 
-    response1 = client.delete('/api/profiles/{}/'.format(profile1.user_id))
-    assert response1.status_code == 204
+    # anon user gets 403 forbidden
+    response = client.delete('/api/profiles/{}/'.format(profile1.user_id))
+    assert response.status_code == 403
+
+    # login as normal_user1, can see own profile
+    client.login(username='user1', password='qwerty123')
+    response = client.delete('/api/profiles/{}/'.format(profile1.user_id))
+    assert response.status_code == 204
+    assert Profile.objects.count() == 1
+    # still cannot delete normal_user2's profile
+    response = client.delete('/api/profiles/{}/'.format(profile1.user_id))
+    assert response.status_code == 404    # 404 because not in queryset
     assert Profile.objects.count() == 1
 
-    response2 = client.delete('/api/profiles/{}/'.format(profile2.user_id))
-    assert response2.status_code == 204
+    # login as normal_user2, can see own profile
+    client.logout()
+    client.login(username='user2', password='qwerty123')
+    response = client.delete('/api/profiles/{}/'.format(profile2.user_id))
+    assert response.status_code == 204
     assert Profile.objects.count() == 0
 
 
-def test_patch_profile(client, create_profiles):
+def test_patch_profile(admin_client, client, create_profiles):
     profile1, profile2 = Profile.objects.all()
 
-    # Changing profile1 existing field
-    client.patch(
+    # anon user gets 403
+    response = client.patch(
         '/api/profiles/{}/'.format(profile1.user_id),
         data=json.dumps({'phone': '9177654321'}),
         content_type='application/json')
+    assert response.status_code == 403
+
+    # login as normal_user1, can see own profile
+    client.login(username='user1', password='qwerty123')
+    response = client.patch(
+        '/api/profiles/{}/'.format(profile1.user_id),
+        data=json.dumps({'phone': '9177654321'}),
+        content_type='application/json')
+    assert response.status_code == 200
+
+    # patch has worked
     profile1 = Profile.objects.first()
     assert profile1.phone == '9177654321'
     # check that other fields remain the same
     assert '{:%Y-%m-%d}'.format(profile1.bday) == '2017-03-17'
 
-    # Changing profile2 currently empty field
-    client.patch(
+    # admin can patch
+    response = admin_client.patch(
         '/api/profiles/{}/'.format(profile2.user_id),
         data=json.dumps({'phone': '9491234567'}),
         content_type='application/json')
+    assert response.status_code == 200
+
     profile2 = Profile.objects.all()[1]
     assert profile2.phone == '9491234567'
     # check that other fields remain empty
--- profiles/views.py
+++ profiles/views.py
@@ -1,9 +1,24 @@
 from rest_framework import viewsets
+from rest_framework.permissions import AllowAny, IsAuthenticated
 
 from .models import Profile
+from .permissions import IsOwnUserOrAdmin
 from .serializers import ProfileSerializer
 
 
 class ProfileViewSet(viewsets.ModelViewSet):
-    queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
+
+    def get_permissions(self):
+        if self.request.method == 'POST':
+            # allow anon users to create profiles
+            return (AllowAny(),)
+        else:
+            return (IsAuthenticated(), IsOwnUserOrAdmin())
+
+    def get_queryset(self):
+        curr_user = self.request.user
+        if curr_user.is_staff:
+            return Profile.objects.all()
+        else:
+            return Profile.objects.filter(user_id=curr_user.id)
