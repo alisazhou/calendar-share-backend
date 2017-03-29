7c0fe14b884f735735eb3a90ec86322aa12dacbc
Alisa Zhou <alisa.zwy@gmail.com>
2017-03-29 13:26:44 -0400

    more efficient database query

--- events/permissions.py
+++ events/permissions.py
@@ -1,8 +1,10 @@
 from rest_framework.permissions import BasePermission
 
+from calendars.models import Calendar
+
 
 class IsMemberOrAdmin(BasePermission):
     def has_object_permission(self, request, view, obj):
         curr_user = request.user
-        is_member = obj.calendar in list(curr_user.calendars_shared.all())
+        is_member = Calendar.objects.filter(id=obj.calendar.id).exists()
         return curr_user.is_staff or is_member
