82c116fd4bbf844e5d9c1aded5e0f0a76c08fb6a
Alisa Zhou <alisa.zwy@gmail.com>
2017-04-05 22:34:55 -0400

    fix Flight and Plan tests using token auth

--- events/tests/flight_view_test.py
+++ events/tests/flight_view_test.py
@@ -1,17 +1,19 @@
 import json
 
+from common.test_helpers import get_user_token
 from events.models import Flight
 from events.tests.event_plugins import check_event_is_instance, get_response_non_field_errors
 
 
 def test_get_flights_list(client, create_flights):
-    # anon user gets 403
+    # anon user gets 401
     response = client.get('/api/flights/')
-    assert response.status_code == 403
+    assert response.status_code == 401
 
     # login as normal_user1 gets 200
-    client.login(username='user1', password='qwerty123')
-    response = client.get('/api/flights/')
+    user1_token = get_user_token(client, 'user1', 'qwerty123')
+    response = client.get(
+        '/api/flights/', HTTP_AUTHORIZATION='Token {}'.format(user1_token))
     assert response.status_code == 200
     # only sees autosaved membership of self
     flights_json = json.loads(response.content.decode())
@@ -23,9 +25,9 @@ def test_get_flights_list(client, create_flights):
     check_event_is_instance(retrieved_flight, flight_instance, serialized=True)
 
     # login as normal_user2 gets 200
-    client.logout()
-    client.login(username='user2', password='qwerty123')
-    response = client.get('/api/flights/')
+    user2_token = get_user_token(client, 'user2', 'qwerty123')
+    response = client.get(
+        '/api/flights/', HTTP_AUTHORIZATION='Token {}'.format(user2_token))
     assert response.status_code == 200
     # only sees autosaved membership of self
     flights_json = json.loads(response.content.decode())
@@ -41,62 +43,76 @@ def test_get_flight_by_id(client, create_flights):
     flight1 = Flight.objects.get(owner__username='user1')
     flight2 = Flight.objects.get(owner__username='user2')
 
-    # anon user gets 403
+    # anon user gets 401
     response = client.get('/api/flights/{}/'.format(flight1.id))
-    assert response.status_code == 403
+    assert response.status_code == 401
 
     # login as normal_user1 gets 200
-    client.login(username='user1', password='qwerty123')
-    response = client.get('/api/flights/{}/'.format(flight1.id))
+    user1_token = get_user_token(client, 'user1', 'qwerty123')
+    response = client.get(
+        '/api/flights/{}/'.format(flight1.id),
+        HTTP_AUTHORIZATION='Token {}'.format(user1_token))
     assert response.status_code == 200
     # sees own flight
     flight_json1 = json.loads(response.content.decode())
     check_event_is_instance(flight_json1, flight1, serialized=True)
     # cannot see normal_user2's flight
-    response = client.get('/api/flights/{}/'.format(flight2.id))
+    response = client.get(
+        '/api/flights/{}/'.format(flight2.id),
+        HTTP_AUTHORIZATION='Token {}'.format(user1_token))
     assert response.status_code == 404
 
     # login as normal_user2 gets 200
-    client.logout()
-    client.login(username='user2', password='qwerty123')
-    response = client.get('/api/flights/{}/'.format(flight2.id))
+    user2_token = get_user_token(client, 'user2', 'qwerty123')
+    response = client.get(
+        '/api/flights/{}/'.format(flight2.id),
+        HTTP_AUTHORIZATION='Token {}'.format(user2_token))
     assert response.status_code == 200
     # sees own flight
     flight_json2 = json.loads(response.content.decode())
     check_event_is_instance(flight_json2, flight2, serialized=True)
     # cannot see normal_user1's flight
-    response = client.get('/api/flights/{}/'.format(flight1.id))
+    response = client.get(
+        '/api/flights/{}/'.format(flight1.id),
+        HTTP_AUTHORIZATION='Token {}'.format(user2_token))
     assert response.status_code == 404
 
 
 def test_post_flights(client, flight1_for_view, flight2_for_view):
-    # anon user gets 403
+    # anon user gets 401
     response = client.post('/api/flights/', flight1_for_view)
-    assert response.status_code == 403
+    assert response.status_code == 401
 
     # login as normal_user1, can create flight to own calendar
-    client.login(username='user1', password='qwerty123')
-    response = client.post('/api/flights/', flight1_for_view)
+    user1_token = get_user_token(client, 'user1', 'qwerty123')
+    response = client.post(
+        '/api/flights/', flight1_for_view,
+        HTTP_AUTHORIZATION='Token {}'.format(user1_token))
     assert response.status_code == 201
     assert Flight.objects.count() == 1
     # check normal_user1 is saved as owner
     flight1 = Flight.objects.get(title='flight 1')
     assert flight1.owner.username == 'user1'
     # cannot create flight to normal_user2's calendar
-    response = client.post('/api/flights/', flight2_for_view)
+    response = client.post(
+        '/api/flights/', flight2_for_view,
+        HTTP_AUTHORIZATION='Token {}'.format(user1_token))
     assert response.status_code == 400    # cal2 is not part of queryset
 
     # login as normal_user2, can create flight to own calendar
-    client.logout()
-    client.login(username='user2', password='qwerty123')
-    response = client.post('/api/flights/', flight2_for_view)
+    user2_token = get_user_token(client, 'user2', 'qwerty123')
+    response = client.post(
+        '/api/flights/', flight2_for_view,
+        HTTP_AUTHORIZATION='Token {}'.format(user2_token))
     assert response.status_code == 201
     assert Flight.objects.count() == 2
     # check normal_user2 is saved as owner
     flight2 = Flight.objects.get(title='flight 2')
     assert flight2.owner.username == 'user2'
     # cannot create flight to normal_user2's calendar
-    response = client.post('/api/flights/', flight1_for_view)
+    response = client.post(
+        '/api/flights/', flight1_for_view,
+        HTTP_AUTHORIZATION='Token {}'.format(user2_token))
     assert response.status_code == 400    # cal1 is not part of queryset
 
 
@@ -104,25 +120,30 @@ def test_delete_flights(client, create_flights):
     flight1 = Flight.objects.get(owner__username='user1')
     flight2 = Flight.objects.get(owner__username='user2')
 
-    # anon user gets 403
+    # anon user gets 401
     response = client.delete('/api/flights/{}/'.format(flight1.id))
-    assert response.status_code == 403
+    assert response.status_code == 401
     assert Flight.objects.count() == 2
 
     # login as normal_user1, can delete own flight
-    client.login(username='user1', password='qwerty123')
-    response = client.delete('/api/flights/{}/'.format(flight1.id))
+    user1_token = get_user_token(client, 'user1', 'qwerty123')
+    response = client.delete(
+        '/api/flights/{}/'.format(flight1.id),
+        HTTP_AUTHORIZATION='Token {}'.format(user1_token))
     assert response.status_code == 204
     assert Flight.objects.count() == 1
     # cannot delete normal_user2's flight
-    response = client.delete('/api/flights/{}/'.format(flight2.id))
+    response = client.delete(
+        '/api/flights/{}/'.format(flight2.id),
+        HTTP_AUTHORIZATION='Token {}'.format(user1_token))
     assert response.status_code == 404
     assert Flight.objects.count() == 1
 
     # login as normal_user2, can delete own flight
-    client.logout()
-    client.login(username='user2', password='qwerty123')
-    response = client.delete('/api/flights/{}/'.format(flight2.id))
+    user2_token = get_user_token(client, 'user2', 'qwerty123')
+    response = client.delete(
+        '/api/flights/{}/'.format(flight2.id),
+        HTTP_AUTHORIZATION='Token {}'.format(user2_token))
     assert response.status_code == 204
     assert Flight.objects.count() == 0
 
@@ -131,19 +152,20 @@ def test_patch_flights(client, create_flights):
     flight1 = Flight.objects.get(owner__username='user1')
     flight2 = Flight.objects.get(owner__username='user2')
 
-    # anon user gets 403
+    # anon user gets 401
     response = client.patch(
         '/api/flights/{}/'.format(flight1.id),
         data=json.dumps({'title': 'new title'}),
         content_type='application/json')
-    assert response.status_code == 403
+    assert response.status_code == 401
 
     # login as normal_user1, can patch own flight
-    client.login(username='user1', password='qwerty123')
+    user1_token = get_user_token(client, 'user1', 'qwerty123')
     response = client.patch(
         '/api/flights/{}/'.format(flight1.id),
         data=json.dumps({'title': 'new title 1'}),
-        content_type='application/json')
+        content_type='application/json',
+        HTTP_AUTHORIZATION='Token {}'.format(user1_token))
     assert response.status_code == 200
     # check only flight title has been updated
     flight1 = Flight.objects.get(owner__username='user1')
@@ -153,17 +175,19 @@ def test_patch_flights(client, create_flights):
     response = client.patch(
         '/api/flights/{}/'.format(flight2.id),
         data=json.dumps({'title': 'new title from user1'}),
-        content_type='application/json')
+        content_type='application/json',
+        HTTP_AUTHORIZATION='Token {}'.format(user1_token))
     assert response.status_code == 404
     flight1 = Flight.objects.get(owner__username='user1')
     assert flight1.title == 'new title 1'
 
     # login as normal_user2, can patch own flight
-    client.login(username='user2', password='qwerty123')
+    user2_token = get_user_token(client, 'user2', 'qwerty123')
     response = client.patch(
         '/api/flights/{}/'.format(flight2.id),
         data=json.dumps({'title': 'new title 2'}),
-        content_type='application/json')
+        content_type='application/json',
+        HTTP_AUTHORIZATION='Token {}'.format(user2_token))
     assert response.status_code == 200
     # check only flight title has been updated
     flight2 = Flight.objects.get(owner__username='user2')
@@ -173,7 +197,8 @@ def test_patch_flights(client, create_flights):
     response = client.patch(
         '/api/flights/{}/'.format(flight1.id),
         data=json.dumps({'title': 'new title from user2'}),
-        content_type='application/json')
+        content_type='application/json',
+        HTTP_AUTHORIZATION='Token {}'.format(user2_token))
     assert response.status_code == 404
     flight2 = Flight.objects.get(owner__username='user2')
     assert flight2.title == 'new title 2'
@@ -181,21 +206,25 @@ def test_patch_flights(client, create_flights):
 
 def test_start_cannot_be_after_end(client, flight2_for_view):
     # login as normal_user2
-    client.login(username='user2', password='qwerty123')
+    user2_token = get_user_token(client, 'user2', 'qwerty123')
 
     # change flight2 start_at to be after end_at
     flight2_for_view['start_at'] = '2017-03-20 04:00'
-    response = client.post('/api/flights/', data=flight2_for_view)
+    response = client.post(
+        '/api/flights/', data=flight2_for_view,
+        HTTP_AUTHORIZATION='Token {}'.format(user2_token))
     assert response.status_code == 400
     non_field_errors = get_response_non_field_errors(response)
     assert 'End time must come after start.' in non_field_errors
 
 
 def test_must_have_airline_and_flight_no_on_post_if_confirmed(client, flight2_for_view):
-    client.login(username='user2', password='qwerty123')
+    user2_token = get_user_token(client, 'user2', 'qwerty123')
     # change flight2 confirmed to True with no airline, results in error
     flight2_for_view['confirmed'] = True
-    response = client.post('/api/flights/', data=flight2_for_view)
+    response = client.post(
+        '/api/flights/', data=flight2_for_view,
+        HTTP_AUTHORIZATION='Token {}'.format(user2_token))
     assert response.status_code == 400
     non_field_errors = get_response_non_field_errors(response)
     assert 'Airline is required' in non_field_errors
@@ -203,7 +232,9 @@ def test_must_have_airline_and_flight_no_on_post_if_confirmed(client, flight2_fo
 
     # add airline to confirmed_flight, results in error re flight_no
     flight2_for_view['airline'] = 'jetblue'
-    response = client.post('/api/flights/', data=flight2_for_view)
+    response = client.post(
+        '/api/flights/', data=flight2_for_view,
+        HTTP_AUTHORIZATION='Token {}'.format(user2_token))
     assert response.status_code == 400
     non_field_errors = get_response_non_field_errors(response)
     assert 'Flight number is required' in non_field_errors
@@ -211,30 +242,36 @@ def test_must_have_airline_and_flight_no_on_post_if_confirmed(client, flight2_fo
 
     # add flight_no to confirmed_flight, no more errors
     flight2_for_view['flight_no'] = 1415
-    response = client.post('/api/flights/', data=flight2_for_view)
+    response = client.post(
+        '/api/flights/', data=flight2_for_view,
+        HTTP_AUTHORIZATION='Token {}'.format(user2_token))
     assert response.status_code == 201
     assert Flight.objects.count() == 1
 
 
 def test_must_have_airline_and_flight_no_on_patch_if_confirmed(
         client, flight1_for_view, flight2_for_view):
-    client.login(username='user1', password='qwerty123')
+    user1_token = get_user_token(client, 'user1', 'qwerty123')
     # create an unconfirmed flight1 with airline and flight_no
     flight1_for_view['confirmed'] = False
-    client.post('/api/flights/', data=flight1_for_view)
+    client.post(
+        '/api/flights/', data=flight1_for_view,
+        HTTP_AUTHORIZATION='Token {}'.format(user1_token))
     flight1_id = Flight.objects.first().id
 
     # confirm flight1, no need for airline and flight_no (already exist)
     response1 = client.patch(
         '/api/flights/{}/'.format(flight1_id),
         data=json.dumps({'confirmed': True}),
-        content_type='application/json')
+        content_type='application/json',
+        HTTP_AUTHORIZATION='Token {}'.format(user1_token))
     assert response1.status_code == 200
 
     # login as user2, create an unconfirmed flight2 with no airline or flight_no
-    client.logout()
-    client.login(username='user2', password='qwerty123')
-    client.post('/api/flights/', data=flight2_for_view)
+    user2_token = get_user_token(client, 'user2', 'qwerty123')
+    client.post(
+        '/api/flights/', data=flight2_for_view,
+        HTTP_AUTHORIZATION='Token {}'.format(user2_token))
     flight2_id = Flight.objects.get(title='flight 2').id
 
     # confirm flight2, need airline and flight_no
@@ -242,7 +279,8 @@ def test_must_have_airline_and_flight_no_on_patch_if_confirmed(
     response2 = client.patch(
         '/api/flights/{}/'.format(flight2_id),
         data=json.dumps(flight2_for_view),
-        content_type='application/json')
+        content_type='application/json',
+        HTTP_AUTHORIZATION='Token {}'.format(user2_token))
     assert response2.status_code == 400
     non_field_errors = get_response_non_field_errors(response2)
     assert 'Airline is required' in non_field_errors
@@ -251,7 +289,8 @@ def test_must_have_airline_and_flight_no_on_patch_if_confirmed(
     response2 = client.patch(
         '/api/flights/{}/'.format(flight2_id),
         data=json.dumps(flight2_for_view),
-        content_type='application/json')
+        content_type='application/json',
+        HTTP_AUTHORIZATION='Token {}'.format(user2_token))
     assert response2.status_code == 400
     non_field_errors = get_response_non_field_errors(response2)
     assert 'Flight number is required' in non_field_errors
@@ -260,5 +299,6 @@ def test_must_have_airline_and_flight_no_on_patch_if_confirmed(
     response2 = client.patch(
         '/api/flights/{}/'.format(flight2_id),
         data=json.dumps(flight2_for_view),
-        content_type='application/json')
+        content_type='application/json',
+        HTTP_AUTHORIZATION='Token {}'.format(user2_token))
     assert response2.status_code == 200
--- events/tests/plan_view_test.py
+++ events/tests/plan_view_test.py
@@ -1,17 +1,19 @@
 import json
 
+from common.test_helpers import get_user_token
 from events.models import Plan
 from events.tests.event_plugins import check_event_is_instance, get_response_non_field_errors
 
 
 def test_get_plans_list(client, create_plans):
-    # anon user gets 403
+    # anon user gets 401
     response = client.get('/api/plans/')
-    assert response.status_code == 403
+    assert response.status_code == 401
 
     # login as user1, gets 200
-    client.login(username='user1', password='qwerty123')
-    response = client.get('/api/plans/')
+    user1_token = get_user_token(client, 'user1', 'qwerty123')
+    response = client.get(
+        '/api/plans/', HTTP_AUTHORIZATION='Token {}'.format(user1_token))
     assert response.status_code == 200
     # sees calendar1 with autosaved membership
     plans_json = json.loads(response.content.decode())
@@ -25,41 +27,50 @@ def test_get_plan_by_id(client, create_plans):
     plan1 = Plan.objects.get(owner__username='user1')
     plan2 = Plan.objects.get(owner__username='user2')
 
-    # anon user gets 403
+    # anon user gets 401
     response = client.get('/api/plans/{}/'.format(plan1.id))
-    assert response.status_code == 403
+    assert response.status_code == 401
 
     # login as user1, gets 200
-    client.login(username='user1', password='qwerty123')
-    response1 = client.get('/api/plans/{}/'.format(plan1.id))
+    user1_token = get_user_token(client, 'user1', 'qwerty123')
+    response1 = client.get(
+        '/api/plans/{}/'.format(plan1.id),
+        HTTP_AUTHORIZATION='Token {}'.format(user1_token))
     assert response1.status_code == 200
     plan1_json = json.loads(response1.content.decode())
     check_event_is_instance(plan1_json, plan1, serialized=True)
     # cannot get user2's plan
-    response2 = client.get('/api/plans/{}/'.format(plan2.id))
-    assert response.status_code == 403
+    response2 = client.get(
+        '/api/plans/{}/'.format(plan2.id),
+        HTTP_AUTHORIZATION='Token {}'.format(user1_token))
+    assert response.status_code == 401
 
     # login as user2 gets 200
-    client.logout()
-    client.login(username='user2', password='qwerty123')
-    response2 = client.get('/api/plans/{}/'.format(plan2.id))
+    user2_token = get_user_token(client, 'user2', 'qwerty123')
+    response2 = client.get(
+        '/api/plans/{}/'.format(plan2.id),
+        HTTP_AUTHORIZATION='Token {}'.format(user2_token))
     assert response2.status_code == 200
     plan2_json = json.loads(response2.content.decode())
     check_event_is_instance(plan2_json, plan2, serialized=True)
     # cannot get user2's plan
-    response1 = client.get('/api/plans/{}/'.format(plan1.id))
-    assert response.status_code == 403
+    response2 = client.get(
+        '/api/plans/{}/'.format(plan1.id),
+        HTTP_AUTHORIZATION='Token {}'.format(user2_token))
+    assert response.status_code == 401
 
 
 def test_post_plans(client, plan1_for_view, plan2_for_view):
-    # anon user gets 403, nothing posted
+    # anon user gets 401, nothing posted
     response = client.post('/api/plans/', plan1_for_view)
-    assert response.status_code == 403
+    assert response.status_code == 401
     assert Plan.objects.count() == 0
 
     # login as user1 gets 200
-    client.login(username='user1', password='qwerty123')
-    response1 = client.post('/api/plans/', plan1_for_view)
+    user1_token = get_user_token(client, 'user1', 'qwerty123')
+    response1 = client.post(
+        '/api/plans/', plan1_for_view,
+        HTTP_AUTHORIZATION='Token {}'.format(user1_token))
     assert response1.status_code == 201
     assert Plan.objects.count() == 1
     # check plan1's owner is normal_user1
@@ -67,9 +78,10 @@ def test_post_plans(client, plan1_for_view, plan2_for_view):
     assert plan1.owner.username == 'user1'
 
     # login as user2 gets 200
-    client.logout()
-    client.login(username='user2', password='qwerty123')
-    response2 = client.post('/api/plans/', plan2_for_view)
+    user2_token = get_user_token(client, 'user2', 'qwerty123')
+    response2 = client.post(
+        '/api/plans/', plan2_for_view,
+        HTTP_AUTHORIZATION='Token {}'.format(user2_token))
     assert response2.status_code == 201
     assert Plan.objects.count() == 2
     # check plan2's owner is normal_user2
@@ -81,24 +93,29 @@ def test_delete_plans(client, create_plans):
     plan1 = Plan.objects.get(owner__username='user1')
     plan2 = Plan.objects.get(owner__username='user2')
 
-    # anon user gets 403, nothing deleted
+    # anon user gets 401, nothing deleted
     response = client.delete('/api/plans/{}/'.format(plan1.id))
-    assert response.status_code == 403
+    assert response.status_code == 401
     assert Plan.objects.count() == 2
 
     # login as normal_user1, can delete own plan
-    client.login(username='user1', password='qwerty123')
-    response = client.delete('/api/plans/{}/'.format(plan1.id))
+    user1_token = get_user_token(client, 'user1', 'qwerty123')
+    response = client.delete(
+        '/api/plans/{}/'.format(plan1.id),
+        HTTP_AUTHORIZATION='Token {}'.format(user1_token))
     assert response.status_code == 204
     assert Plan.objects.count() == 1
     # cannot delete user2's plan
-    response = client.delete('/api/plans/{}/'.format(plan2.id))
+    response = client.delete(
+        '/api/plans/{}/'.format(plan2.id),
+        HTTP_AUTHORIZATION='Token {}'.format(user1_token))
     assert response.status_code == 404
 
-    # login as normal_user1, can delete own plan
-    client.logout()
-    client.login(username='user2', password='qwerty123')
-    response = client.delete('/api/plans/{}/'.format(plan2.id))
+    # login as normal_user2, can delete own plan
+    user2_token = get_user_token(client, 'user2', 'qwerty123')
+    response = client.delete(
+        '/api/plans/{}/'.format(plan2.id),
+        HTTP_AUTHORIZATION='Token {}'.format(user2_token))
     assert response.status_code == 204
     assert Plan.objects.count() == 0
 
@@ -106,19 +123,20 @@ def test_delete_plans(client, create_plans):
 def test_patch_plans(client, create_plans):
     plan1 = Plan.objects.get(owner__username='user1')
 
-    # anon user gets 403
+    # anon user gets 401
     response = client.patch(
         '/api/plans/{}/'.format(plan1.id),
         data=json.dumps({'title': 'new title'}),
         content_type='application/json')
-    assert response.status_code == 403
+    assert response.status_code == 401
 
     # login as normal_user1, gets 200
-    client.login(username='user1', password='qwerty123')
+    user1_token = get_user_token(client, 'user1', 'qwerty123')
     response = client.patch(
         '/api/plans/{}/'.format(plan1.id),
         data=json.dumps({'title': 'new title for plan 1'}),
-        content_type='application/json')
+        content_type='application/json',
+        HTTP_AUTHORIZATION='Token {}'.format(user1_token))
     assert response.status_code == 200
 
     plan1 = Plan.objects.get(owner__username='user1')
@@ -132,8 +150,10 @@ def test_start_cannot_be_after_end(client, plan2_for_view):
     plan2_for_view['start_at'] = '2017-04-20 04:00'
 
     # login as normal_user2
-    client.login(username='user2', password='qwerty123')
-    response = client.post('/api/plans/', data=plan2_for_view)
+    user2_token = get_user_token(client, 'user2', 'qwerty123')
+    response = client.post(
+        '/api/plans/', data=plan2_for_view,
+        HTTP_AUTHORIZATION='Token {}'.format(user2_token))
     assert response.status_code == 400
     non_field_errors = get_response_non_field_errors(response)
     assert 'End time must come after start.' in non_field_errors
