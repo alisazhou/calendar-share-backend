3ffa5a8e7ccb4f1e47709e90fd842e68e1ca2c09
Alisa Zhou <alisa.zwy@gmail.com>
2017-04-05 22:22:57 -0400

    fix Membership tests using token auth

--- memberships/tests/view_test.py
+++ memberships/tests/view_test.py
@@ -2,6 +2,7 @@ from django.contrib.auth import get_user_model
 import json
 
 from calendars.models import Calendar
+from common.test_helpers import get_user_token
 from memberships.models import Membership
 from profiles.models import Profile
 
@@ -19,14 +20,15 @@ def check_mbship_dict_is_instance(mbship_dict, mbship_instance):
     assert '/api/profiles/{}/'.format(member_id) in mbship_dict['member']
 
 
-def test_get_memberships_list(admin_client, client, create_calendars):
-    # anon user gets 403
+def test_get_memberships_list(admin_user, client, create_calendars):
+    # anon user gets 401
     response = client.get('/api/memberships/')
-    assert response.status_code == 403
+    assert response.status_code == 401
 
     # login as normal_user1 and gets 200
-    client.login(username='user1', password='qwerty123')
-    response = client.get('/api/memberships/')
+    user1_token = get_user_token(client, 'user1', 'qwerty123')
+    response = client.get(
+        '/api/memberships/', HTTP_AUTHORIZATION='Token {}'.format(user1_token))
     assert response.status_code == 200
     # only sees its own memberships
     mbships_json = json.loads(response.content.decode())
@@ -36,9 +38,9 @@ def test_get_memberships_list(admin_client, client, create_calendars):
     check_mbship_dict_is_instance(retrieved_mbship, saved_mbship)
 
     # login as normal_user2 and gets 200
-    client.logout()
-    client.login(username='user2', password='qwerty123')
-    response = client.get('/api/memberships/')
+    user2_token = get_user_token(client, 'user2', 'qwerty123')
+    response = client.get(
+        '/api/memberships/', HTTP_AUTHORIZATION='Token {}'.format(user2_token))
     assert response.status_code == 200
     # only sees its own memberships
     mbships_json = json.loads(response.content.decode())
@@ -48,46 +50,60 @@ def test_get_memberships_list(admin_client, client, create_calendars):
     check_mbship_dict_is_instance(retrieved_mbship, saved_mbship)
 
     # admin sees all memberships
-    response = admin_client.get('/api/memberships/')
+    admin_token = get_user_token(client, 'admin', 'password')
+    response = client.get(
+        '/api/memberships/', HTTP_AUTHORIZATION='Token {}'.format(admin_token))
     assert response.status_code == 200
     mbships_json = json.loads(response.content.decode())
     assert len(mbships_json) == 2
 
 
 def test_get_membership_by_id(
-        admin_client, client, normal_user1, normal_user2, create_calendars):
+        admin_user, client, normal_user1, normal_user2, create_calendars):
     saved_mbship1 = Membership.objects.get(member_id=normal_user1.id)
     saved_mbship2 = Membership.objects.get(member_id=normal_user2.id)
 
-    # anon user gets 403
+    # anon user gets 401
     response = client.get('/api/memberships/{}/'.format(saved_mbship1.id))
-    assert response.status_code == 403
+    assert response.status_code == 401
 
     # login as normal_user1 and gets 200
-    client.login(username='user1', password='qwerty123')
-    response = client.get('/api/memberships/{}/'.format(saved_mbship1.id))
+    user1_token = get_user_token(client, 'user1', 'qwerty123')
+    response = client.get(
+        '/api/memberships/{}/'.format(saved_mbship1.id),
+        HTTP_AUTHORIZATION='Token {}'.format(user1_token))
     assert response.status_code == 200
     retrieved_mbship = json.loads(response.content.decode())
     check_mbship_dict_is_instance(retrieved_mbship, saved_mbship1)
     # normal_user1 cannot get normal_user2's membership
-    response = client.get('/api/memberships/{}/'.format(saved_mbship2.id))
+    response = client.get(
+        '/api/memberships/{}/'.format(saved_mbship2.id),
+        HTTP_AUTHORIZATION='Token {}'.format(user1_token))
     assert response.status_code == 404
 
     # login as normal_user2 and gets 200
-    client.logout()
-    client.login(username='user2', password='qwerty123')
-    response = client.get('/api/memberships/{}/'.format(saved_mbship2.id))
+    user2_token = get_user_token(client, 'user2', 'qwerty123')
+    response = client.get(
+        '/api/memberships/{}/'.format(saved_mbship2.id),
+        HTTP_AUTHORIZATION='Token {}'.format(user2_token))
     assert response.status_code == 200
     retrieved_mbship = json.loads(response.content.decode())
     check_mbship_dict_is_instance(retrieved_mbship, saved_mbship2)
     # normal_user2 cannot get normal_user1's membership
-    response = client.get('/api/memberships/{}/'.format(saved_mbship1.id))
+    response = client.get(
+        '/api/memberships/{}/'.format(saved_mbship1.id),
+        HTTP_AUTHORIZATION='Token {}'.format(user2_token))
     assert response.status_code == 404
 
     # admin sees both
-    response = admin_client.get('/api/memberships/{}/'.format(saved_mbship1.id))
+    admin_token = get_user_token(client, 'admin', 'password')
+    response = client.get(
+        '/api/memberships/{}/'.format(saved_mbship1.id),
+        HTTP_AUTHORIZATION='Token {}'.format(admin_token))
     assert response.status_code == 200
-    response = admin_client.get('/api/memberships/{}/'.format(saved_mbship2.id))
+    response = client.get(
+        '/api/memberships/{}/'.format(saved_mbship2.id),
+        HTTP_AUTHORIZATION='Token {}'.format(admin_token))
     assert response.status_code == 200
 
 
@@ -104,27 +120,34 @@ def test_post_memberships(client, create_calendars, create_profiles):
         'calendar': '/api/calendars/{}/'.format(cal2.id),
         'member': '/api/profiles/{}/'.format(complete_profile.user_id)}
 
-    # anon user gets 403
+    # anon user gets 40l
     response = client.post('/api/memberships/', mbship1_data)
-    assert response.status_code == 403
+    assert response.status_code == 401
 
     # login as normal_user1, gets 200
-    client.login(username='user1', password='qwerty123')
-    response = client.post('/api/memberships/', mbship1_data)
+    user1_token = get_user_token(client, 'user1', 'qwerty123')
+    response = client.post(
+        '/api/memberships/', mbship1_data,
+        HTTP_AUTHORIZATION='Token {}'.format(user1_token))
     assert response.status_code == 201
     assert Membership.objects.count() == 3    # already 2 from create_calendars
     # but cannot post membership with calendar owned by normal_user2
-    response = client.post('/api/memberships/', mbship2_data)
+    response = client.post(
+        '/api/memberships/', mbship2_data,
+        HTTP_AUTHORIZATION='Token {}'.format(user1_token))
     assert response.status_code == 400    # cal2 not in queryset
 
     # login as normal_user2 and gets 200
-    client.logout()
-    client.login(username='user2', password='qwerty123')
-    response = client.post('/api/memberships/', mbship2_data)
+    user2_token = get_user_token(client, 'user2', 'qwerty123')
+    response = client.post(
+        '/api/memberships/', mbship2_data,
+        HTTP_AUTHORIZATION='Token {}'.format(user2_token))
     assert response.status_code == 201
     assert Membership.objects.count() == 4
     # but cannot post membership with calendar owned by normal_user2
-    response = client.post('/api/memberships/', mbship1_data)
+    response = client.post(
+        '/api/memberships/', mbship1_data,
+        HTTP_AUTHORIZATION='Token {}'.format(user2_token))
     assert response.status_code == 400    # cal1 not in queryset
 
 
@@ -132,23 +155,28 @@ def test_delete_memberships(client, normal_user1, normal_user2, create_calendars
     mbship1 = Membership.objects.get(member_id=normal_user1.id)
     mbship2 = Membership.objects.get(member_id=normal_user2.id)
 
-    # anon user gets 403
+    # anon user gets 40l
     response = client.delete('/api/memberships/{}/'.format(mbship1.id))
-    assert response.status_code == 403
+    assert response.status_code == 401
 
     # login as normal_user1, gets 200
-    client.login(username='user1', password='qwerty123')
-    response = client.delete('/api/memberships/{}/'.format(mbship1.id))
+    user1_token = get_user_token(client, 'user1', 'qwerty123')
+    response = client.delete(
+        '/api/memberships/{}/'.format(mbship1.id),
+        HTTP_AUTHORIZATION='Token {}'.format(user1_token))
     assert response.status_code == 204
     assert Membership.objects.count() == 1
     # still can't delete normal_user2's membership
-    response = client.delete('/api/memberships/{}/'.format(mbship2.id))
+    response = client.delete(
+        '/api/memberships/{}/'.format(mbship2.id),
+        HTTP_AUTHORIZATION='Token {}'.format(user1_token))
     assert response.status_code == 404
 
     # login as normal_user2 and gets 200
-    client.logout()
-    client.login(username='user2', password='qwerty123')
-    response = client.delete('/api/memberships/{}/'.format(mbship2.id))
+    user2_token = get_user_token(client, 'user2', 'qwerty123')
+    response = client.delete(
+        '/api/memberships/{}/'.format(mbship2.id),
+        HTTP_AUTHORIZATION='Token {}'.format(user2_token))
     assert response.status_code == 204
     assert Membership.objects.count() == 0
 
@@ -157,19 +185,20 @@ def test_patch_membership(client, normal_user1, normal_user2, create_calendars):
     mbship1 = Membership.objects.get(member_id=normal_user1.id)
     mbship2 = Membership.objects.get(member_id=normal_user2.id)
 
-    # anon user gets 403
+    # anon user gets 401
     response = client.patch(
         '/api/memberships/{}/'.format(mbship1.id),
         data=json.dumps({'color_hex': '111111'}),
         content_type='application/json')
-    assert response.status_code == 403
+    assert response.status_code == 401
 
     # login as normal_user1, gets 200
-    client.login(username='user1', password='qwerty123')
+    user1_token = get_user_token(client, 'user1', 'qwerty123')
     response = client.patch(
         '/api/memberships/{}/'.format(mbship1.id),
         data=json.dumps({'color_hex': '11111F'}),
-        content_type='application/json')
+        content_type='application/json',
+        HTTP_AUTHORIZATION='Token {}'.format(user1_token))
     assert response.status_code == 200
     mbship1 = Membership.objects.get(member_id=normal_user1.id)
     assert mbship1.color_hex == '11111F'
@@ -177,7 +206,8 @@ def test_patch_membership(client, normal_user1, normal_user2, create_calendars):
     response = client.patch(
         '/api/memberships/{}/'.format(mbship2.id),
         data=json.dumps({'color_hex': '22222F'}),
-        content_type='application/json')
+        content_type='application/json',
+        HTTP_AUTHORIZATION='Token {}'.format(user1_token))
     assert response.status_code == 404
 
 
@@ -193,9 +223,10 @@ def test_cannot_use_repeated_color_in_one_calendar(client, create_calendars):
         'member': '/api/profiles/{}/'.format(user1_id)}
 
     # login as normal_user2, as only existing members can add new members
-    client.login(username='user2', password='qwerty123')
-    print(mbship1_data['calendar'])
-    response = client.post('/api/memberships/', mbship1_data)
+    user2_token = get_user_token(client, 'user2', 'qwerty123')
+    response = client.post(
+        '/api/memberships/', mbship1_data,
+        HTTP_AUTHORIZATION='Token {}'.format(user2_token))
     assert response.status_code == 400
     err_msg = json.loads(response.content.decode())
     assert 'Members cannot have the same color.' in err_msg['non_field_errors']
